ea02e4cb187f25d9165a697f288e490ca75c73af
yooncheol <oyc1230@4usoft.kr>
2023-02-13 16:56:46 +0900

    2023-02-13 프론트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
